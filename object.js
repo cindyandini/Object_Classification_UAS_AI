9c294a393504a3d4a8846863ef258e14b9acc8fd
cindyandini <57897008+cindyandini@users.noreply.github.com>
2020-06-15 19:30:32 +0800

    Add files via upload

---
+++ object.js
@@ -0,0 +1,74 @@
+// The video
+let video;
+// For displaying the label
+let label = "waiting...";
+// The classifier
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/jMhW5TrJN/';
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
+function setup() {
+  createCanvas(640, 520);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+// STEP 2 classify the videeo!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function draw() {
+  background(0);
+
+  // Draw the video
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, height - 16);
+
+  // Pick an emoji, the "default" is train
+  let emoji = "";
+  if (label == "Sisir") {
+    emoji = "";
+  } else if (label == "Kacamata") {
+    emoji = "";
+  } else if (label == "Gunting") {
+    emoji = "";
+  }else if (label == "Jam Tangan") {
+    emoji = "";
+  }else if (label == "Sendok") {
+    emoji = "";
+  }else if (label == "Buku") {
+    emoji = "";
+  }else if (label == "Botol") {
+    emoji = "";
+  }
+
+  // Draw the emoji
+  textSize(256);
+  text(emoji, width / 2, height / 2);
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  // Something went wrong!
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label and classify again!
+  label = results[0].label;
+  classifyVideo();
+}
